8b89d1334e5a8c5f017011a934ed73dd0322bb0c
arkadijstul <005ark@gmail.com>
2023-12-08 17:38:59 +0300

    test of player + / filename

--- backend/web/core/utils/file.py
+++ backend/web/core/utils/file.py
@@ -1,10 +1,10 @@
 def get_song_filepath(instance, filename):
-    return f'song/{instance.name}_{instance.id}'
+    return f'song/{instance.name}_{instance.id}/{filename}'
 
 
 def get_analysis_filepath(instance, filename):
-    return f'analysis/{instance.name}_{instance.id}'
+    return f'analysis/{instance.name}_{instance.id}/{filename}'
 
 
 def get_about_filepath(instance, filename):
-    return f'analysis/{instance.name}_{instance.id}'
+    return f'analysis/{instance.name}_{instance.id}/{filename}'
