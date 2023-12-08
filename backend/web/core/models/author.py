500fe09c0533113c4a6962d1c902ed5b5bda8e90
arkadijstul <005ark@gmail.com>
2023-12-08 19:21:30 +0300

    исправленная циклическая ошибка

--- backend/web/core/models/author.py
+++ backend/web/core/models/author.py
@@ -2,59 +2,56 @@ from django.db import models
 from django.dispatch import receiver
 from django.urls import reverse
 from django.db.models.signals import pre_save
-
-from core.models import Century
+from .century import Century
 
 
 def get_century_choices():
-
     century_choices = []
     for century in range(-1, 22):
-        if century<0:
+        if century < 0:
             x = f'{abs(century)} до н.э.'
         elif century == 0:
             continue
         else:
             x = f'{century}'
-        century_choices.append([century,x])
+        century_choices.append([century, x])
     return century_choices
 
+
 class TypeChoices:
-        MUSIC='music'
-        WRITER='writer'
-        ARTIST='artist'
-        PHILOSOPHER='philosopher'
-        MUSICIAN='musician'
-        DIRECTOR='director'
-        LECTOR='lector'
-        SPIRITUAL='spiritual'
-        
+    MUSIC = 'music'
+    WRITER = 'writer'
+    ARTIST = 'artist'
+    PHILOSOPHER = 'philosopher'
+    MUSICIAN = 'musician'
+    DIRECTOR = 'director'
+    LECTOR = 'lector'
+    SPIRITUAL = 'spiritual'
+
 
 author_type_choices = [
-            [TypeChoices.MUSIC,'Музыкант'],
-            [TypeChoices.WRITER,'Литератор'],
-            [TypeChoices.ARTIST,'Художник'],
-            [TypeChoices.PHILOSOPHER,'Философ'],
-            [TypeChoices.MUSICIAN,'Музыкант'],
-            [TypeChoices.LECTOR,'Лектор'],
-            [TypeChoices.DIRECTOR,'Режиссер'],
-            [TypeChoices.SPIRITUAL,'Духоносный'],
-            ]
-
+    [TypeChoices.MUSIC, 'Музыкант'],
+    [TypeChoices.WRITER, 'Литератор'],
+    [TypeChoices.ARTIST, 'Художник'],
+    [TypeChoices.PHILOSOPHER, 'Философ'],
+    [TypeChoices.MUSICIAN, 'Музыкант'],
+    [TypeChoices.LECTOR, 'Лектор'],
+    [TypeChoices.DIRECTOR, 'Режиссер'],
+    [TypeChoices.SPIRITUAL, 'Духоносный'],
+]
 
 
 class Author(models.Model):
-    
     name = models.CharField(
         max_length=255,
         verbose_name='Имя'
     )
-        #dob = models.TextField(
-        #date = models.DateTimeField(default=datetime.now, blank=True)
-        #verbose_name='дата',
-        #blank=True, 
-        #null=True
-    #)
+    # dob = models.TextField(
+    # date = models.DateTimeField(default=datetime.now, blank=True)
+    # verbose_name='дата',
+    # blank=True,
+    # null=True
+    # )
     category = models.ForeignKey(
         to='Category',
         on_delete=models.SET_NULL,
@@ -62,11 +59,11 @@ class Author(models.Model):
         null=True,
         blank=True,
     )
-    
+
     text = models.TextField(
         max_length=80000,
         verbose_name='текст',
-        blank=True, 
+        blank=True,
         null=True
     )
     year = models.IntegerField(
@@ -77,14 +74,14 @@ class Author(models.Model):
 
     website = models.URLField(
         max_length=250,
-        blank=True, 
+        blank=True,
         null=True
     )
 
     text_preview = models.TextField(
         max_length=3000,
         verbose_name='текст_превью',
-        blank=True, 
+        blank=True,
         null=True
     )
     slug = models.CharField(
@@ -93,14 +90,13 @@ class Author(models.Model):
         null=True,
         unique=True
     )
-    type = models.CharField( 
+    type = models.CharField(
         choices=author_type_choices,
         max_length=255,
         verbose_name='тип',
         null=True,
-       
+
     )
-  
 
     image = models.ImageField(
         upload_to='images',
@@ -115,22 +111,20 @@ class Author(models.Model):
         blank=True,
     )
 
-
     def __str__(self):
         return f'{self.name}'
-    
 
     def get_absolute_url(self):
-        return reverse('author_detail',kwargs={'slug':self.slug})
-    
+        return reverse('author_detail', kwargs={'slug': self.slug})
+
     class Meta:
-        #verbose_name_plural = "Категории"
-        ordering=['century']
-    
+        # verbose_name_plural = "Категории"
+        ordering = ['century']
+
     @staticmethod
     def get_search_field():
         return 'name__icontains'
-    
+
     teacher = models.ManyToManyField(
         to="self",
         blank=True,
@@ -139,7 +133,7 @@ class Author(models.Model):
         to="self",
         blank=True,
     )
-    
+
     associate = models.ManyToManyField(
         to="self",
         blank=True,
@@ -150,16 +144,19 @@ class Author(models.Model):
 
         blank=True,
     )
+
     class Meta:
         ordering = ["name"]
+
+
 @receiver(pre_save, sender=Author)
-def func_name(sender, instance: Author, save=None,  **kwargs):
-    if instance.year>0:
-        century = instance.year/100+1
+def func_name(sender, instance: Author, save=None, **kwargs):
+    if instance.year > 0:
+        century = instance.year / 100 + 1
     else:
-        century = instance.year / 100-1
-    x = Century.objects.filter(value=century).first
-    instance.century=x
+        century = instance.year / 100 - 1
+    x = Century.objects.filter(value=century).first()
+    instance.century = x
 
 
 
